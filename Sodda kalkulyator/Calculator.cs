2da7214cff9fa7d9e04c1d33f6f7fbca627cabcb
alijo <alijo@ALIJONOV>
2023-11-24 19:49:57 +0500

    Add project files.

---
+++ Sodda kalkulyator/Calculator.cs
@@ -0,0 +1,59 @@
+﻿
+
+namespace Sodda_kalkulyator
+{
+    internal class Calculator
+    {
+        public static double Calculate(string nums)
+        {
+            double natija = 0;
+            string[] s = nums.Split();
+            if (s.Length != 3)
+            {
+                Console.WriteLine("O'zgaruvchilar kutilgan sonda emas!");
+                return 0;
+            }
+            double x = 0, y = 0;
+            try
+            {
+                x = double.Parse(s[0]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            if (!(s[1] == "+" || s[1] == "-" || s[1] == "/" || s[1] == "*"))
+            {
+                Console.WriteLine("Amallardan birini tanlang : +, -, *, / ");
+                return 0 ;
+            }
+            try
+            {
+                y = double.Parse(s[2]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            switch (s[1])
+            {
+                case "+": natija = x + y; break;
+                case "-": natija = x - y; break;
+                case "*": natija = x * y; break;
+                case "/":
+                    {
+                        try
+                        {
+                            natija = x / y;
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                    }
+                    break;
+            }
+            return natija;
+        }
+    }
+}
---
+++ Sodda kalkulyator/Program.cs
@@ -0,0 +1,9 @@
+﻿using Sodda_kalkulyator;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string? s = Console.ReadLine();
+        Console.WriteLine("Natija = " + Calculator.Calculate(s));
+    }
+}
